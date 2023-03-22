c026aadc1e5b985d7f859074a3070564259877eb
rfilano <rfilano6@gmail.com>
2023-03-22 19:14:12 -0400

    testing gitignore

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ from PIL import Image
 import pytesseract
 import fitz
 
-pdf_files = [f for f in os.listdir(
-    ".") if os.path.isfile(os.path.join(".", f))]
-for file in pdf_files:  # look at every file in the current directory
-    if file.endswith('.pdf'):  # if it is a PDF, use it
+def pdf_to_image(file):
         doc = fitz.open(file)
         i = 0
+        output_pages = []
         for page in doc:
             i += 1
             pix = page.get_pixmap()
             output = file[:-4] + "_page_" + i.__str__() + ".png"
             pix.save(output)
+            output_pages.append(output)
         doc.close()
+        return output_pages
 
-png_files = [f for f in os.listdir(
-    ".") if os.path.isfile(os.path.join(".", f))]
-for file in png_files:
-    if file.endswith('.png'):  # if it is a PDF, use it
-        print(file[:-4] + " as string: ")
-        print(pytesseract.image_to_string(Image.open(file[:-4] + '.png')))
-        open(file[:-4] + ".txt",
-             "w").write(pytesseract.image_to_string(Image.open(file[:-4] + '.png')))
+def images_to_text(pages):
+        for page in pages:
+            print(pytesseract.image_to_string(Image.open(page)))
+
+def gpt_call(audience, pdf_text):
+      return ("Here's a test for " + audience + ". Please provide sample answers for a great student, an average student, and a poor student. Evaluate the difficulty and divergence of each question.\n" + pdf_text)
+
+if __name__ == "__main__":
+      pngs = pdf_to_image("Sample2.pdf")
+      pdf_text = images_to_text(pngs)
+
+#python quizinspector.py quiz.pdf
+#print("Who is the audience for this quiz?")
+#audience = user input
+#"Here is a quiz for {audience}. Provide sample answers and a difficulty level for each question"
