aace3b9fe53ea5829d4df13e8edf5a44f1d94d61
rfilano <rfilano6@gmail.com>
2023-03-29 22:28:19 -0400

    Prototype 1 Complete

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
-import os
+import openai
 from PIL import Image
-
+import argparse
 import pytesseract
 import fitz
 
-pdf_files = [f for f in os.listdir(
-    ".") if os.path.isfile(os.path.join(".", f))]
-for file in pdf_files:  # look at every file in the current directory
-    if file.endswith('.pdf'):  # if it is a PDF, use it
-        doc = fitz.open(file)
-        i = 0
-        for page in doc:
-            i += 1
-            pix = page.get_pixmap()
-            output = file[:-4] + "_page_" + i.__str__() + ".png"
-            pix.save(output)
-        doc.close()
+with open('secrets.txt') as f:
+    openai.api_key = f.read()
 
-png_files = [f for f in os.listdir(
-    ".") if os.path.isfile(os.path.join(".", f))]
-for file in png_files:
-    if file.endswith('.png'):  # if it is a PDF, use it
-        print(file[:-4] + " as string: ")
-        print(pytesseract.image_to_string(Image.open(file[:-4] + '.png')))
-        open(file[:-4] + ".txt",
-             "w").write(pytesseract.image_to_string(Image.open(file[:-4] + '.png')))
+
+def pdf_to_image(file):
+    doc = fitz.open(file)
+    i = 0
+    output_pages = []
+    for page in doc:
+        i += 1
+        pix = page.get_pixmap()
+        output = file[:-4] + "_page_" + i.__str__() + ".png"
+        pix.save(output)
+        output_pages.append(output)
+    doc.close()
+    return output_pages
+
+
+def images_to_text(pages):
+    pdf_text = ""
+    for page in pages:
+        pdf_text = pdf_text + pytesseract.image_to_string(Image.open(page))
+    return pdf_text
+
+
+def gpt_call(audience, pdf_text):
+    prompt = "Here's a test for " + audience + \
+        " students. Please provide sample answers for a great student, an average student, and a poor student. Evaluate the difficulty and divergence of each question.\n" + pdf_text
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": "test"}],
+        temperature=0
+    )
+    return (response["choices"][0]["message"]["content"])
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdf_name")
+    args = parser.parse_args()
+    pngs = pdf_to_image(args.pdf_name)
+    pdf_text = images_to_text(pngs)
+    print("Please format your response as 'Grade Level Subject'\n For example: 3rd grade math.")
+    audience = input("What is the target audience? ")
+    print(gpt_call(audience, pdf_text))
